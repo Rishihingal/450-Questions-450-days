7fef798e537b4c805ee37262d585ded57a5e86ea
Rishabh Hingal <rishihingal@gmail.com>
2022-04-19 20:23:25 +0530

    Day-1 Question 1

---
+++ ReverseArray.java
@@ -0,0 +1,8 @@
+class ReverseArray {
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5,6};
+        for(int i=A.length-1;i>=0;i--){
+             System.out.print(A[i]+" ");
+        }       
+    }
+}
